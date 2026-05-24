5fc637148b6559ff407b4514987278fc236ae5d7
Matias Sarlinga <matiassarlinga14@gmail.com>
2026-05-24 20:23:59 -0300

    "runneragents" started

---
+++ runneragents.py
@@ -0,0 +1,31 @@
+import random
+from agents import Agent
+from environment import SimulatedSensor, SimulatedActuator, SimulatedEnvironment
+from runnerworld import RunnerChaseEnvironment, Role, ObstacleType, CORRECT_ACTION
+
+class PositionSensor(SimulatedSensor):
+    def sense(self):
+        r = self._env.get_property(self._agent_id, "position")
+        return r.get("position", 0)
+
+class NextObstacleSensor(SimulatedSensor):
+    def sense(self):
+        r = self._env.get_property(self._agent_id, "next_obstacle")
+        return r.get("next_obstacle", ObstacleType.NONE.value)
+
+class DistanceSensor(SimulatedSensor):
+    def sense(self):
+        r = self._env.get_property(self._agent_id, "distance")
+        return r.get("distance", -1)
+
+class ErrorSensor(SimulatedSensor):
+    def sense(self):
+        own = self._env.get_property(self._agent_id, "errors_self").get("errors_self", 0)
+        opp = self._env.get_property(self._agent_id, "errors_opponent").get("errors_opponent", 0)
+        return own, opp
+
+class GameOverSensor(SimulatedSensor):
+    def sense(self):
+        r = self._env.get_property(self._agent_id, "game_over")
+        return r.get("game_over", False)
+
